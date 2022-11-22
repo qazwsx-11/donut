cb0bc2f4f4af5f0d3fe53ac4dd8d4e6136bee0d1
qazwsx-11 <90948757+qazwsx-11@users.noreply.github.com>
2022-11-22 12:40:25 +0530

    Update spinningboy.py

--- spinningboy.py
+++ spinningboy.py
@@ -9,8 +9,6 @@ time.sleep(1)
 print(".")
 time.sleep(1)
 print(".")
-time.sleep(1)
-print(".")
 time.sleep(3)
 
 def main():
@@ -57,7 +55,7 @@ def main():
                         
                 if 0<y<height and 0<x<width and z[o] < mess:
                     z[o]=mess
-                    screen[o]=".,-~:;=!*#$@"[N if N>0 else 0]
+                    screen[o]="AllahuAllah"[N if N>0 else 0]
 
         # print 
         os.system(clear)
